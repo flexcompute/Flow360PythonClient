0bec8c3eb73a6579e5c1d52e13884ce3012c8941
Lei Zheng <zhenglei2010fall@gmail.com>
2020-12-07 19:00:35 -0500

    refactor: Support new Transform features

--- flow360client/tests/testMeshcgns.py
+++ flow360client/tests/testMeshcgns.py
@@ -1,17 +1,12 @@
 
 from flow360client import mesh, NewCase, NewCaseListWithPhase
 
-resp = mesh.AddMesh('cylinder.cgns', ["fluid/wall"], ['tag1'], 'cgns', '', 'release-20.3.1')
+resp = mesh.AddMesh('cylinder.cgns', ["fluid/wall"], ['tag1'], 'cgns', '', None)
 print(resp)
 meshId = resp['meshId']
 print(meshId)
 
 mesh.UploadMesh(meshId, 'data/cylinder.cgns')
-
-caseId = NewCase(meshId=meshId, config='data/cyclinder_case.json', caseName='case_stuck', tags=[],
-            priority='high', parentId=None)
-
-print(caseId)
 
 caseIds = NewCaseListWithPhase(meshId=meshId, config='data/cyclinder_case.json', caseName='case_stuck', tags=[],
             priority='high', parentId=None, phaseCount=3)
